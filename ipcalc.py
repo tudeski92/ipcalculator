bf464b8ff68f8079a4a5c704b342c3eaf455513c
Andrzej Jurek <a.jurek@gmail.com>
2021-04-07 22:31:00 +0200

    Removing unnecessary comments

--- ipcalc.py
+++ ipcalc.py
@@ -15,9 +15,7 @@ app.config['SECRET_KEY'] = 'gshfkjgh1kj2hkj4hkj'
 dogs = ['dog-1.jpg', 'dog-2.jpg', 'dog-3.jpg', 'dog-4.jpg']
 
 
-#huehuehue
-#ELOOOOOOOOOOOOOO
-#another commend
+
 def dec_to_bin(decimal):
     mylist = []
     while decimal != 1:
